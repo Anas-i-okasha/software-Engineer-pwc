3aacb7ca5d4bc9671bd1db235bac3d54dac1de0d
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-03-05 00:17:28 +0200

    login backend finished

--- Backend/controller/main-controller.js
+++ Backend/controller/main-controller.js
@@ -1,11 +1,10 @@
 const mysql = require('../db');
 const bcrypt = require("bcrypt");
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 
-
+/* Create account fistly we need to check if the user already have account or not     */
     const userSignup=  (req , res)=>{
-
         const {name  , email , password}=req.body
         mysql.query(`SELECT * FROM users WHERE email = ?` , [email] , async (error , result , field)=>{
             if(error){
@@ -18,7 +17,6 @@ const bcrypt = require("bcrypt");
                 const newUser = req.body;
                 newUser.password = await bcrypt.hash(password , parseInt(process.env.SALT) )
         // if the user does't exist in our database will create new account to make him reach our dashboard 
-            //  const sql=`INSERT INTO users (name , age , email , password) VALUES (? , ? , ? , ?)`;
              mysql.query( `INSERT INTO users SET ? `,{name:name , email:email , password:newUser.password} , (err , results , field)=>{
                  if(err){
                      console.log(err)
@@ -32,7 +30,37 @@ const bcrypt = require("bcrypt");
 
 
 
+    /*  the user login and check if the email and hash password is match redirected to dashbord */
+    const userLogin = async (req , res)=>{
+        try{
+            const {email , password}=req.body
+    
+            mysql.query(`SELECT * FROM users WHERE email = ?` , [email] , async (error , result , field)=>{
+                if(error){
+                    console.log('Err', error)
+                }
+                if(result.length===0){
+                    res.status(404).json({massege:'the user is not found '})
+                }else{
+                    if(await bcrypt.compare(password , result[0].password)){
+                        res.json('login successful')
+                        
+                    }else{
+                        console.log('invalid username and password')
+                        res.status(404).json({massege:'invalid username or password'})
+                    }
+                }
+            })
+               /*  if there are error  */
+        } catch (err){
+             console.log('ERR',err)
+        }
+    
+        
+    }
+
 
 module.exports={
     userSignup,
+    userLogin
 }
--- Backend/routes/main-routes.js
+++ Backend/routes/main-routes.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const mainRouter = express.Router();
 
-const {userSignup}=require('../controller/main-controller')
+const {userSignup , userLogin}=require('../controller/main-controller')
 
+mainRouter.post('/',userLogin)
 mainRouter.post('/signup' , userSignup)
 
 
